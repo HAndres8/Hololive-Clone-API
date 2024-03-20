37674bfbd744d62644aabe022603e84bbc805da8
HAndres8 <andresfhernandez999@gmail.com>
2024-03-19 19:49:00 -0500

    Added immutable property to names in TalentSchema

--- src/schema/TalentSchema.ts
+++ src/schema/TalentSchema.ts
@@ -2,8 +2,8 @@ import { model, Schema } from "mongoose";
 import TalentEntity from "../entity/TalentEntity";
 
 const TalentSchema = new Schema<TalentEntity>({
-    name: { type:String, required:true, trim:true, unique:true },
-    nameJP: { type:String, required:true, trim:true, unique:true },
+    name: { type:String, required:true, trim:true, unique:true, immutable:true },
+    nameJP: { type:String, required:true, trim:true, unique:true, immutable:true },
     isAlum: { type:Boolean, required:true, trim:true },
     url: { type:String, required:true, trim:true }
 },
